f8eb74d8e0c829fd2c28ca4234fc415c4fa84c2c
William Jefferies <willjefferies10@gmail.com>
2018-05-09 16:16:41 -0600

    Another

--- models/index.js
+++ models/index.js
@@ -2,9 +2,8 @@ var mongoose = require("mongoose");
 if (process.env.NODE_ENV == "production") {
   mongoose.connect(process.env.MLAB_URL)
 } else {
-  mongoose.connect("mongodb://localhost/whenpresident");
+  mongoose.connect("mongodb://localhost/slapsave");
 }
 
 module.exports.Machine = require("./machine");
 
-mongodb://slapsave:slapsave@ds251819.mlab.com:51819/echobase
